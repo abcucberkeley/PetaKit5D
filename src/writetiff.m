201255ebc0e378f1da4f90b0bd0ea2c30962924f
Xiongtao Ruan <xruan@Xiongtao-MacBook-Pro.local>
2020-08-06 22:17:13 +0800

    update writetiff.m as a wrapper for saveastiff.m to support bigtiff format

--- src/writetiff.m
+++ src/writetiff.m
@@ -1,56 +1,29 @@
-%writetiff(img, filepath, varargin) writes a TIFF stack using libtiff
-% Stores TIFFs as 64-bit
-
-% Francois Aguet, 05/21/2013
-
 function writetiff(img, filepath, varargin)
+% wrapper for saveastiff.m to support big tiff format, and keep
+% compatibility of previous script for the decrapted writetiff. 
+% 
+% Author: Xiongtao Ruan (07/30/2020)
+
 
 ip = inputParser;
 ip.CaseSensitive = false;
-ip.addParamValue('Compression', 'lzw', @(x) any(strcmpi(x, {'none', 'lzw'})));
-ip.addParamValue('Mode', 'libtiff', @(x) any(strcmpi(x, {'libtiff', 'imwrite'})));
+ip.addParameter('Compression', 'lzw', @(x) any(strcmpi(x, {'none', 'lzw'})));
+ip.addParameter('Mode', 'libtiff', @(x) any(strcmpi(x, {'libtiff', 'imwrite'})));
 ip.parse(varargin{:});
 
-t = Tiff(filepath,'w');
+pr = ip.Results;
 
-[ny,nx,N] = size(img);
-w = whos('img');
-
-switch lower(ip.Results.Mode)
-    case 'libtiff'
-        tagstruct.ImageLength = ny;
-        tagstruct.ImageWidth = nx;
-        tagstruct.Photometric = Tiff.Photometric.MinIsBlack;
-        switch lower(ip.Results.Compression)
-            case 'none'
-                tagstruct.Compression = Tiff.Compression.None;
-            case 'lzw'
-                tagstruct.Compression = Tiff.Compression.LZW;
-        end
-        tagstruct.BitsPerSample = 8*w.bytes/nx/ny/N;
-        tagstruct.SamplesPerPixel = 1;
-        tagstruct.RowsPerStrip = 16;
-        if any(strcmpi(w.class, {'uint8', 'uint16', 'uint32', 'uint64'}))
-            tagstruct.SampleFormat = Tiff.SampleFormat.UInt;
-        elseif any(strcmpi(w.class, {'int8', 'int16', 'int32', 'int64'}))
-            tagstruct.SampleFormat = Tiff.SampleFormat.Int;
-        else
-            tagstruct.SampleFormat = Tiff.SampleFormat.IEEEFP;
-        end
-        tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-        tagstruct.SubFileType = Tiff.SubFileType.Page;
-        tagstruct.PageNumber = [1 N];
-        tagstruct.Software = 'MATLAB';
-        
-        for k = 1:N;
-            t.setTag(tagstruct);
-            t.write(img(:,:,k));
-            t.writeDirectory();
-        end
-        t.close();
-    case 'imwrite'
-        imwrite(img(:,:,1), filepath, 'tif', 'compression' , ip.Results.Compression);
-        for k = 2:N
-            imwrite(img(:,:,k), filepath, 'tif', 'compression' , ip.Results.Compression, 'writemode', 'append');
-        end
+options = struct();
+switch pr.Compression
+    case 'none'
+        options.compress = 'no';
+    case 'lzw'
+        options.compress = 'lzw';
 end
+
+options.message = false;
+options.overwrite = true;
+saveastiff(img, filepath, options);
+
+end
+
