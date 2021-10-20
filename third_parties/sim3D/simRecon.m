886143654b73d814d873aebb04457b092843fe12
Matthew Mueller <mrm.mue@gmail.com>
2021-10-20 16:03:00 -0700

    Fixed apodize in sim recon

--- third_parties/sim3D/simRecon.m
+++ third_parties/sim3D/simRecon.m
@@ -1,5 +1,5 @@
 function [Data_r_space] = simRecon(data, otf, varargin)
-% This code will generate a assemble a super-resolved SIM image from a deskewed 
+% This code will generate a assemble a super-resolved SIM image from a deskewed
 % dataset and OTF acquired with 5-phase lattice SIM data.
 
 ip = inputParser;
@@ -28,7 +28,7 @@ ip.addParameter('lattice_angle', [pi/2], @isnumeric); %Angle parellel to pattern
 ip.addParameter('phase_step', .232, @isnumeric); %Phase step in microns
 ip.addParameter('pxl_dim_data', [0.11,0.11,0.3*sind(32.4)], @isnumeric); %Voxel dimensions of the image in microns - note, stored as [dy,dx,dz]
 ip.addParameter('pxl_dim_PSF', [0.11,0.11,0.2*sind(32.4)], @isnumeric); %Voxel dimensions of the PSF in microns - note, stored as [dy,dx,dz]
-ip.addParameter('Background', 105, @isnumeric); 
+ip.addParameter('Background', 105, @isnumeric);
 
 ip.addParameter('useGPU', true, @islogical);
 
@@ -88,7 +88,7 @@ nz_data=nimgs_data/(nphases*norientations);
 
 if(useGPU)
     dk_data=gpuArray(1./([ny_data,nx_data,nz_data].*pxl_dim_data));
-else 
+else
     dk_data=1./([ny_data,nx_data,nz_data].*pxl_dim_data);
 end
 
@@ -134,8 +134,8 @@ clear O
 cylmask=cylMask(NA_det,wvl_em,NA_ext,wvl_ext,nimm,norders,ny_data,nx_data,nz_data,dk_data,islattice,useGPU);
 %toc
 
-% Notes: right now, this masking is just used when determining the starting 
-% phase and modulation depth. There are likely other ways to mask the OTFs (e.g. 
+% Notes: right now, this masking is just used when determining the starting
+% phase and modulation depth. There are likely other ways to mask the OTFs (e.g.
 % via SNR).
 
 %Separate data information orders by solving the linear system of equations for each pixel
@@ -208,46 +208,46 @@ for jj=1:norientations
         p_vec_guess(kk,:,jj)=order*k_shift_scaling.*[sin(lattice_angle(jj)),cos(lattice_angle(jj)),0];
         
         for qq=1:2
-        %Shift image frequency information - D˜m(k+mp)
-        shift_Dk_sep=fourierShift3D(Dk_sep(:,:,:,kk,jj),p_vec_guess(kk,:,jj),useGPU);
-        
-        %Shift transfer function - O_m(k+mp)
-        shift_Om=fourierShift3D(O_scaled(:,:,:,kk,jj),p_vec_guess(kk,:,jj),useGPU);
-        
-        %Shift mask - we use imtranslate here because for small images,
-        %fourier shifting can wrap around the image
-        % cylmask_shift=imtranslate_function(double(cylmask(:,:,:,kk)),[p_vec_guess(kk,2,jj),p_vec_guess(kk,1,jj),p_vec_guess(kk,3,jj)],'FillValues',0);
-        % cylmask_shift_0=abs(cylmask_shift)>.5; %Get rid of non-logical values due to interpolation
-        
-        sz = size(cylmask, [1, 2, 3]);
-        cylmask_shift = false(sz);
-        shift = round([p_vec_guess(kk,1,jj),p_vec_guess(kk,2,jj),p_vec_guess(kk,3,jj)]);
-        so = max(1, 1 - shift);
-        to = min(sz, sz - shift);
-        sn = max(1, shift + 1);
-        tn = min(sz, sz + shift);
-        cylmask_shift(sn(1) : tn(1), sn(2) : tn(2), sn(3) : tn(3)) = cylmask(so(1) : to(1), so(2) : to(2), so(3) : to(3), kk);
-        
-        %Overlap mask with the zero-information component
-        overlap_mask= cylmask(:,:,:,3)&cylmask_shift;
-        
-
-% Next, scale the shifted copies by either the shifted or non-shifted OTF as 
-% per equations 9* and 9**. 
-
-        DmO0=shift_Dk_sep.*O_scaled(:,:,:,ceil(norders/2),jj); %D˜m(k+mp)O_0(k) - eqn (9*)
-        D0Om=Dk_sep(:,:,:,ceil(norders/2),jj).*shift_Om; %D˜0(k)O_m(k+mp) - eqn (9**)
-        
-        %Right now, we only do 1 round of shift vector refinement. In
-        %practice, we could iterate.
-        if qq==1
+            %Shift image frequency information - D˜m(k+mp)
+            shift_Dk_sep=fourierShift3D(Dk_sep(:,:,:,kk,jj),p_vec_guess(kk,:,jj),useGPU);
+            
+            %Shift transfer function - O_m(k+mp)
+            shift_Om=fourierShift3D(O_scaled(:,:,:,kk,jj),p_vec_guess(kk,:,jj),useGPU);
+            
+            %Shift mask - we use imtranslate here because for small images,
+            %fourier shifting can wrap around the image
+            % cylmask_shift=imtranslate_function(double(cylmask(:,:,:,kk)),[p_vec_guess(kk,2,jj),p_vec_guess(kk,1,jj),p_vec_guess(kk,3,jj)],'FillValues',0);
+            % cylmask_shift_0=abs(cylmask_shift)>.5; %Get rid of non-logical values due to interpolation
+            
+            sz = size(cylmask, [1, 2, 3]);
+            cylmask_shift = false(sz);
+            shift = round([p_vec_guess(kk,1,jj),p_vec_guess(kk,2,jj),p_vec_guess(kk,3,jj)]);
+            so = max(1, 1 - shift);
+            to = min(sz, sz - shift);
+            sn = max(1, shift + 1);
+            tn = min(sz, sz + shift);
+            cylmask_shift(sn(1) : tn(1), sn(2) : tn(2), sn(3) : tn(3)) = cylmask(so(1) : to(1), so(2) : to(2), so(3) : to(3), kk);
+            
+            %Overlap mask with the zero-information component
+            overlap_mask= cylmask(:,:,:,3)&cylmask_shift;
+            
+            
+            % Next, scale the shifted copies by either the shifted or non-shifted OTF as
+            % per equations 9* and 9**.
+            
+            DmO0=shift_Dk_sep.*O_scaled(:,:,:,ceil(norders/2),jj); %D˜m(k+mp)O_0(k) - eqn (9*)
+            D0Om=Dk_sep(:,:,:,ceil(norders/2),jj).*shift_Om; %D˜0(k)O_m(k+mp) - eqn (9**)
+            
+            %Right now, we only do 1 round of shift vector refinement. In
+            %practice, we could iterate.
+            if qq==1
                 [transform,~,~,~] = MaskedTranslationRegistration2D_fit(abs(sum(D0Om,3)),abs(sum(DmO0,3)),max(overlap_mask,[],3),max(overlap_mask,[],3),.5,useGPU);
                 transform=[transform(2),transform(1),0];
                 p_vec_guess(kk,:,jj)=p_vec_guess(kk,:,jj)-transform;
                 transform_tot=transform_tot+transform;
                 norm(transform);
                 %maxC;
-        end
+            end
         end
         
         %['lattice period estimate from order ', num2str(order), ' = ', num2str(abs(order)/norm(p_vec_guess(kk,:).*dk_data)), ' microns'];
@@ -296,7 +296,7 @@ if(useGPU)
     B_PLS = gpuArray(B_PLS);
     Dk_sep_scaled = gpuArray(zeros(ny_data + padrange(1) * 2, nx_data + padrange(2) * 2,nz_data + padrange(3) * 2));
     denom = gpuArray(zeros(ny_data + padrange(1) * 2, nx_data + padrange(2) * 2,nz_data + padrange(3) * 2));
-else 
+else
     Dk_sep_scaled = zeros(ny_data + padrange(1) * 2, nx_data + padrange(2) * 2,nz_data + padrange(3) * 2);
     denom = zeros(ny_data + padrange(1) * 2, nx_data + padrange(2) * 2,nz_data + padrange(3) * 2);
 end
@@ -336,11 +336,11 @@ clear shift_denom
 %toc
 
 
-% The next step is to shift these scaled information components back into their 
-% correct locations in frequency space and then add them together into the final 
-% dataset. This is pretty straightforward since we already know the correct shift 
-% vectors. To account for this shift, we need a larger frequency-space (which 
-% is basically super-sampling with smaller pixels in real space) compared to our 
+% The next step is to shift these scaled information components back into their
+% correct locations in frequency space and then add them together into the final
+% dataset. This is pretty straightforward since we already know the correct shift
+% vectors. To account for this shift, we need a larger frequency-space (which
+% is basically super-sampling with smaller pixels in real space) compared to our
 % original image. For now, we'll use a super-sampling factor of 2.
 
 %'assembling final dataset'
@@ -353,16 +353,16 @@ if(useGPU)
     p_vec_guess = gpuArray(p_vec_guess);
 else
     % Data_k_space = padarray(zeros(ny_data,nx_data,nz_data),padrange,'both');
-    Data_k_space = zeros(ny_data + padrange(1) * 2, nx_data + padrange(2) * 2,nz_data + padrange(3) * 2);    
+    Data_k_space = zeros(ny_data + padrange(1) * 2, nx_data + padrange(2) * 2,nz_data + padrange(3) * 2);
 end
 %Shift information components and assemble final image
 for jj=1:norientations
-%Assemble final dataset
-for ii=1:norders
-    big_kspace_shifted=fourierShift3D(Dk_sep_scaled(:,:,:,ii,jj),p_vec_guess(ii,:,jj),useGPU);
-    Data_k_space=Data_k_space+big_kspace_shifted;
-    clear big_kspace_shifted
-end
+    %Assemble final dataset
+    for ii=1:norders
+        big_kspace_shifted=fourierShift3D(Dk_sep_scaled(:,:,:,ii,jj),p_vec_guess(ii,:,jj),useGPU);
+        Data_k_space=Data_k_space+big_kspace_shifted;
+        clear big_kspace_shifted
+    end
 end
 
 clear Dk_sep_scaled
@@ -372,26 +372,18 @@ clear Dk_sep_scaled
 %disp("apodizeEllipse")
 %tic
 if apodize
-[Data_k_space_apodized,~] = apodizeEllipse(Data_k_space,dk_data,p_vec_guess,lattice_angle,NA_det,nimm,NA_ext,wvl_em,wvl_ext,islattice,useGPU);
+    [Data_k_space_apodized,~] = apodizeEllipse(Data_k_space,dk_data,p_vec_guess,lattice_angle,NA_det,nimm,NA_ext,wvl_em,wvl_ext,islattice,useGPU);
+    clear Data_k_space
+    %Transform final dataset back into real space
+    Data_r_space=real(fftshift(fftn(ifftshift(Data_k_space_apodized))))*prod(dk_data);
+else
+    Data_r_space=real(fftshift(fftn(ifftshift(Data_k_space))))*prod(dk_data);
 end
-clear Data_k_space
 %toc
 
-%Transform final dataset back into real space
-%disp("transform data back to real space")
-%tic
-Data_r_space=real(fftshift(fftn(ifftshift(Data_k_space_apodized))))*prod(dk_data);
-%toc
-
-%disp("write to disk (write3Dtiff)")
-%tic
 %TESTING
 %if(useGPU)
 %    Data_r_space = gather(Data_r_space);
 %end
-
-%Write the data to disk
-%write3Dtiff(single(abs(Data_r_space)),['/clusterfs/fiona/matthewmueller/20210830SimRecon3D/2020_11_12(Code_for_Kitware)/2020_11_12(Code_for_Kitware)/data/siRecon_5phase_small_CPUOPTIMIZED.tif'])
-%toc
 
 end
