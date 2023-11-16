a85cf18cf6110b4c1065c966d19c7684756a8254
ruanxt <xruan@berkeley.edu>
2023-11-15 17:32:32 -0800

    also remove nodisplay option for linux and mac mcc to enable figure plotting and recompile linux mcc version

--- mcc/compile_mccMaster.m
+++ mcc/compile_mccMaster.m
@@ -12,7 +12,8 @@ if ismac
     end
     copyfile([fpath '/../microscopeDataProcessing/io/cpp-tiff/mac/*.dylib'],'/Applications/LLSM5DToolsMCC/');
     copyfile([fpath '/../microscopeDataProcessing/io/cpp-zarr/mac/*.dylib'],'/Applications/LLSM5DToolsMCC/');
-    mcc -v -R -nodisplay -C -d /Applications/LLSM5DToolsMCC -m mccMaster.m
+    % mcc -v -R -nodisplay -C -d /Applications/LLSM5DToolsMCC -m mccMaster.m
+    mcc -v -C -d /Applications/LLSM5DToolsMCC -m mccMaster.m
 
     cd('/Applications/LLSM5DToolsMCC');
     system("sed -i '' '20i\'$'\n''  DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/Applications/LLSM5DToolsMCC; '$'\n''' run_mccMaster.sh");
@@ -34,7 +35,7 @@ elseif isunix
     end
     % mcc -v -R -nodisplay -R -singleCompThread -d linux -m mccMaster.m
     % mcc -v -R -nodisplay -R -nojvm -d linux -m mccMaster.m
-    mcc -v -R -nodisplay -d linux -m mccMaster.m
+    mcc -v -d linux -m mccMaster.m
 
     cd('linux');
     system('sed -i ''21i\  LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${exe_dir}/../../microscopeDataProcessing/io/cpp-tiff/linux;'' run_mccMaster.sh');
