bb405b1195613c7e615f379ff195b00d775dea65
ruanxt <xruan@berkeley.edu>
2023-12-15 16:55:12 -0800

    fix bug for mcc parser for tiff to zarr function; recompile linux mcc runtimes

--- mcc/XR_tiffToZarr_wrapper_parser.m
+++ mcc/XR_tiffToZarr_wrapper_parser.m
@@ -81,7 +81,7 @@ end
 if ischar(tileOutBbox)
     tileOutBbox = str2num(tileOutBbox);
 end
-if ischar(processFunPath)
+if ischar(processFunPath) && strcmp(processFunPath(1), '{')
     processFunPath = eval(processFunPath);
 end
 if ischar(parseCluster)
