d839a8cdfb25371c11ac6e5cc5dfb35b659eee22
ruanxt <xruan@berkeley.edu>
2024-08-14 23:56:58 -0700

    update mex compile script

--- microscopeDataProcessing/compileAllMexFiles.m
+++ microscopeDataProcessing/compileAllMexFiles.m
@@ -40,6 +40,14 @@ else
     mex -v CC="/usr/local/bin/gcc-13" CXX="/usr/local/bin/g++-13" COPTIMFLAGS="-O3 -DNDEBUG" CFLAGS='$CFLAGS -O3 -fopenmp' LDFLAGS='$LDFLAGS -O3 -fopenmp' skewed_space_interp_defined_stepsize_mex.c
 end
 
+% volume_deskew_rotate_warp_mex
+if(~ismac)
+    mex -v COPTIMFLAGS="-O3 -DNDEBUG" CFLAGS='$CFLAGS -O3 -fopenmp' LDFLAGS='$LDFLAGS -O3 -fopenmp' volume_deskew_rotate_warp_mex.cpp
+else
+    mex -v CC="/usr/local/bin/gcc-13" CXX="/usr/local/bin/g++-13" COPTIMFLAGS="-O3 -DNDEBUG" CFLAGS='$CFLAGS -O3 -fopenmp' CXXFLAGS='-fno-common -arch arm64 -mmacosx-version-min=10.15 -fexceptions -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -std=c++11 -O3 -fopenmp -DMATLAB_DEFAULT_RELEASE=R2017b  -DUSE_MEX_CMD   -DMATLAB_MEX_FILE' LDFLAGS='$LDFLAGS -O3 -fopenmp' LDFLAGS='$LDFLAGS -O3 -fopenmp' feather_blending_3d_with_indexing_mex.cpp
+end
+
+
 cd('../../stitch/mex');
 
 % feather_blending_3d_mex
